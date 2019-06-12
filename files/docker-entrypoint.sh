39574f802cb3323c3012d1804a24687ad4877dc5
Rat <atkinson21@gmail.com>
2019-06-11 18:22:56 -0700

    Forgot to change script

--- files/docker-entrypoint.sh
+++ files/docker-entrypoint.sh
@@ -3,18 +3,13 @@ set -euo pipefail
 
 id
 
-cd /home/container/factorio
-echo "Changed to /home/container/factorio"
-
-FACTORIO_VOL=/home/container/factorio
+FACTORIO_VOL=/factorio
 mkdir -p "$FACTORIO_VOL"
 mkdir -p "$SAVES"
 mkdir -p "$CONFIG"
 mkdir -p "$MODS"
 mkdir -p "$SCENARIOS"
 mkdir -p "$SCRIPTOUTPUT"
-
-echo "Making directories"
 
 if [ ! -f "$CONFIG/rconpw" ]; then
   # Generate a new RCON password if none exists
@@ -42,12 +37,12 @@ fi
 
 if [ "$(id -u)" = '0' ]; then
   # Update the User and Group ID based on the PUID/PGID variables
-  usermod -o -u "$PUID" container
-  groupmod -o -g "$PGID" container
-  # Take ownership of container data if running as root
-  chown -R container:container "$FACTORIO_VOL"
-  # Drop to the container user
-  SU_EXEC="su-exec container"
+  usermod -o -u "$PUID" factorio
+  groupmod -o -g "$PGID" factorio
+  # Take ownership of factorio data if running as root
+  chown -R factorio:factorio "$FACTORIO_VOL"
+  # Drop to the factorio user
+  SU_EXEC="su-exec factorio"
 else
   SU_EXEC=""
 fi
@@ -56,33 +51,21 @@ NRSAVES=$( find -L "$SAVES" -iname \*.zip -mindepth 1 | wc -l )
 if [ "$NRSAVES" -eq 0 ]; then
   # Generate a new map if no save ZIPs exist
   $SU_EXEC /opt/factorio/bin/x64/factorio \
-    --create "$SAVES/$SAVE_NAME.zip" \
+    --create "$SAVES/_autosave1.zip" \
     --map-gen-settings "$CONFIG/map-gen-settings.json" \
     --map-settings "$CONFIG/map-settings.json"
 fi
 
-# Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
-
-# Run the Server
-${MODIFIED_STARTUP}
-
 # shellcheck disable=SC2086
-#exec $SU_EXEC /opt/factorio/bin/x64/factorio \
-#  --port "{{SERVER_PORT}}" \
-#  --start-server-load-latest \
-#  --server-settings "$CONFIG/server-settings.json" \
-#  --server-banlist "$CONFIG/server-banlist.json" \
-#  --rcon-port "$RCON_PORT" \
-#  --server-whitelist "$CONFIG/server-whitelist.json" \
-#  --use-server-whitelist \
-#  --server-adminlist "$CONFIG/server-adminlist.json" \
-#  --rcon-password "$(cat "$CONFIG/rconpw")" \
-#  --server-id /factorio/config/server-id.json \
-#  "$@"
-
-
-
-  #./bin/x64/factorio --port {{SERVER_PORT}} --server-settings data/server-settings.json --start-server {{SAVE_NAME}}.zip
-  #exec $SU_EXEC /opt/factorio/bin/x64/factorio --port "{{SERVER_PORT}}" --start-server-load-latest --server-settings "{{CONFIG}}/server-settings.json" --server-banlist "{{CONFIG}}/server-banlist.json" --rcon-port "$RCON_PORT" --server-whitelist "{{CONFIG}}/server-whitelist.json" --use-server-whitelist --server-adminlist "{{CONFIG}}/server-adminlist.json" --rcon-password "$(cat "{{CONFIG}}/rconpw")" --server-id /factorio/config/server-id.json "$@"
+exec $SU_EXEC /opt/factorio/bin/x64/factorio \
+  --port "$PORT" \
+  --start-server-load-latest \
+  --server-settings "$CONFIG/server-settings.json" \
+  --server-banlist "$CONFIG/server-banlist.json" \
+  --rcon-port "$RCON_PORT" \
+  --server-whitelist "$CONFIG/server-whitelist.json" \
+  --use-server-whitelist \
+  --server-adminlist "$CONFIG/server-adminlist.json" \
+  --rcon-password "$(cat "$CONFIG/rconpw")" \
+  --server-id /factorio/config/server-id.json \
+  "$@"
